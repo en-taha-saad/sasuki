67f455b2cfd239ab05bc4bdaf7d9a0fbbb8e1b9a
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-03-08 11:28:34 +0300

    Create init_login_module.dart

---
+++ lib/app/init_screens_dependencies/init_login_module.dart
@@ -0,0 +1,40 @@
+import 'package:get_it/get_it.dart';
+import 'package:sasuki/app/init_screens_dependencies/init_app_module.dart';
+import 'package:sasuki/domain/repository/repository.dart';
+import 'package:sasuki/domain/usecase/captcha_usecase/captcha_usecase.dart';
+import 'package:sasuki/domain/usecase/captcha_usecase/responsecaptcha_usecase.dart';
+import 'package:sasuki/domain/usecase/login_usecase/login_usecase.dart';
+
+initLoginModule() async {
+  // login module, only login dependencies are here
+
+  // login use case instance
+  if (!GetIt.I.isRegistered<CaptchaUseCase>()) {
+    instance.registerFactory<CaptchaUseCase>(
+      () => CaptchaUseCase(instance<Repository>()),
+    );
+  }
+  if (!GetIt.I.isRegistered<LoginUseCase>()) {
+    instance.registerFactory<LoginUseCase>(
+      () => LoginUseCase(instance<Repository>()),
+    );
+  }
+
+  if (!GetIt.I.isRegistered<ResponseCaptchaUseCase>()) {
+    instance.registerFactory<ResponseCaptchaUseCase>(
+      () => ResponseCaptchaUseCase(instance<Repository>()),
+    );
+  }
+  // TODO check the model
+  // if (!GetIt.I.isRegistered<LoginViewModel>()) {
+  //   // login view model instance
+  //   instance.registerFactory<LoginViewModel>(
+  //     () => LoginViewModel(
+  //       instance<LoginUseCase>(),
+  //       instance<CaptchaUseCase>(),
+  //       instance<ResponseCaptchaUseCase>(),
+  //       instance<SelectedServerUsecase>(),
+  //     ),
+  //   );
+  // }
+}
