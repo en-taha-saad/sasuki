b7039fddd2ace3cb9045ffe13a9c2fdefb6ea82f
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-03-08 11:27:19 +0300

    Update splash_view.dart

--- lib/presentation/splash/splash_view.dart
+++ lib/presentation/splash/splash_view.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:sasuki/app/app_inits_funs/app_prefs.dart';
@@ -8,100 +7,8 @@ import 'package:sasuki/app/app_inits_funs/constants.dart';
 import 'package:sasuki/app/init_screens_dependencies/init_app_module.dart';
 import 'package:sasuki/app/init_screens_dependencies/init_choose_server_module.dart';
 import 'package:sasuki/app/resources/other_managers/assets_manager.dart';
-
-class SplashView0 extends StatefulWidget {
-  const SplashView0({Key? key}) : super(key: key);
-  @override
-  // ignore: library_private_types_in_public_api
-  _SplashView0State createState() => _SplashView0State();
-}
-
-class _SplashView0State extends State<SplashView0> {
-  Timer? _timer;
-  final AppPreferences _appPrefs = instance<AppPreferences>();
-  _startDelay() {
-    _timer = Timer(const Duration(seconds: Constants.splashDelay), _goNext);
-  }
-
-  _goNext() async {
-    String? token = await _appPrefs.getToken();
-    _appPrefs.getIsUserLoggedIn().then((isUserLoggedIn) {
-      if (isUserLoggedIn == true) {
-        if (token != null) {
-          bool isExpired = JwtDecoder.isExpired(token);
-          debugPrint("token = $token");
-          debugPrint("isExpired = $isExpired");
-          if (isExpired) {
-            _appPrefs.removeIsUserLoggedInStatus();
-            _appPrefs.removeToken();
-            initChooseServerModule();
-            // navigate to login
-            // TODO nav to the screen
-            // Nav.replaceTo(context, Routes.loginRoute);
-            return;
-          }
-        }
-        // TODO nav to the screen
-        // Nav.replaceTo(context, Routes.dashboardRoute);
-        return;
-      } else {
-        _appPrefs.getSelectedServer().then((isChosedServer) {
-          if (isChosedServer != null) {
-            initChooseServerModule();
-            // navigate to login
-            // TODO nav to the screen
-            // Nav.replaceTo(context, Routes.loginRoute);
-          } else {
-            // TODO nav to the screen
-            // Nav.replaceTo(context, Routes.chooseServerRoute);
-          }
-        });
-      }
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _startDelay();
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: ColorManager.primary,
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        // backgroundColor: ColorManager.primary,
-        // elevation: AppSize.s0,
-        systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarIconBrightness: Brightness.light,
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(),
-          const Center(
-              // child: Image.asset(
-              //   ImageAssets.sasukiLogo,
-              //   height: AppSize.s138,
-              //   width: AppSize.s265,
-              // ),
-              ),
-          // Center(child: SvgPicture.asset(ImageAssets.backgroundSvg)),
-        ],
-      ),
-    );
-  }
-}
+import 'package:sasuki/app/resources/routes_manager/nav_funcs.dart';
+import 'package:sasuki/app/resources/routes_manager/routes.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -122,12 +29,6 @@ class _SplashViewState extends State<SplashView> {
 
   _startDelay() {
     _timer = Timer(const Duration(seconds: Constants.splashDelay), _goNext);
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
   }
 
   _goNext() async {
@@ -168,10 +69,15 @@ class _SplashViewState extends State<SplashView> {
         // TODO nav to the screen
         // Nav.replaceTo(context, Routes.loginRoute);
       } else {
-        // TODO nav to the screen
-        // Nav.replaceTo(context, Routes.chooseServerRoute);
+        Nav.replaceTo(context, Routes.chooseServerRoute);
       }
     });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
