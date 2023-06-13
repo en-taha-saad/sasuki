7a99b1775f2f69f851c30d437ae15dd6cc2afbd3
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-13 14:13:45 +0300

    add firebase crachylist in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'dart:ui';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:sasuki/app/app_inits_funs/my_app.dart';
 import 'package:sasuki/app/shared_funs/pre_init_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   await preRun();
   //
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
